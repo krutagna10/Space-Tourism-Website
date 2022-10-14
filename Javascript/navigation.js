9591b5e68be5fa5a6d23969d2c960b5dcec2d5a9
Krutagna Patel <kpatel.kp7016@gmail.com>
2022-10-14 22:47:47 +0530

    .

--- Javascript/navigation.js
+++ Javascript/navigation.js
@@ -20,14 +20,10 @@ function update() {
 
 
 // Mobile Navigation
-let menuButton = document.querySelector('.menu-button');
-let closeButton = document.querySelector('.close-button');
-menuButton.addEventListener('click', () => {
-    document.querySelector('.primary-header').classList.add('navigation-open');
-})
+let mobileNavigationButton = document.querySelector('.mobile-navigation-button');
 
-closeButton.addEventListener('click', () => {
-    document.querySelector('.primary-header').classList.remove('navigation-open');
+mobileNavigationButton.addEventListener('click', () => {
+    document.querySelector('.primary-header').classList.toggle('navigation-open');
+    mobileNavigationButton.classList.toggle('active');
 })
-
 
